dd6b9e610b72d79412294dbffb8c48481bf2e7f7
nazar-si <nazari61@yandex.ru>
2023-07-16 15:29:17 +0300

    Added localization library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/tailwind.css"
-
 import { AppProps } from "next/app"
+import i18n from "libs/i18n";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
